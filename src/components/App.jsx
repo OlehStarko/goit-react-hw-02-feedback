6f54d096300ab44d7a3b291b933dc290c6216359
OlehStarko <starartds@gmail.com>
2022-10-21 21:43:24 +0300

    ok

---
+++ src/components/App.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import css from '../css/feedback.module.css';
+import Section from '../components/Section/Section';
+import Statistics from '../components/Statistics/Statistics';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  onClickFeedback = event => {
+    const label = event.target.textContent;
+
+    this.setState(prevState => ({ [label]: (prevState[label] += 1) }));
+  };
+
+  countTotalFeedback = () => {
+    const total = Object.keys(this.state).reduce(
+      (acc, value) => acc + this.state[value],
+      0
+    );
+    return total;
+  };
+
+  countPositivePercentage = () => {
+    const percent = Math.round(
+      (this.state.good * 100) / this.countTotalFeedback()
+    );
+
+    return percent;
+  };
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <div>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={this.state}
+            onClickFeedback={this.onClickFeedback}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={this.countTotalFeedback}
+            positivePercentage={this.countPositivePercentage}
+          />
+        </Section>
+      </div>
+    );
+  }
+}
+
+export default App;
